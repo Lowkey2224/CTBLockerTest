a07f402bd6299429698ed0f32e9025346fc05876
jenz <jenz@silpion.de>
2016-02-25 21:41:00 +0100

    init

---
+++ CTBTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LokiMcFly\CTBLocker\Test;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @author Marcus Jenz
+ */
+class CTBTest extends PHPUnit_Framework_TestCase
+{
+
+    public function test(){
+        $this->assertTrue(true);
+    }
+
+}
